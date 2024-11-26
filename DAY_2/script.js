4b4bfbe00d0a892e566dbdd7c494a833275656f2
aniket-dev-ai <2414501734@mujonline.edu.in>
2024-11-26 11:57:08 +0530

    task 5

--- DAY_2/script.js
+++ DAY_2/script.js
@@ -7,3 +7,13 @@
 // let b = prompt("enter age")
 
 // console.log(`hello ${a} , you are ${b} year old`);
+
+// let a = parseFloat(prompt("Enter the value of Legnth :"));
+// let b = parseFloat(prompt("Enter the value of width:"));
+
+// console.log(`Legnth = ${a}, width = ${b}, and their area is = ${a*b}`);
+
+// let a = Number(prompt("Enter the value of a:"));
+// let b = Number(prompt("Enter the value of b:"));
+
+// console.log((a>b)? `a = ${a} is larger` : ` b = ${b} is larger`)
