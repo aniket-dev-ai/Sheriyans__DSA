830b08dad4e0833c7e6884b92e8b25c0d811ad60
aniket-dev-ai <2414501734@mujonline.edu.in>
2024-11-28 12:23:19 +0530

    task 4

--- Day_4/script.js
+++ Day_4/script.js
@@ -1,7 +1,7 @@
 // Reverse a  Number
 
-let n = 12345;
-let r = 0;
+var n = 12345;
+var r = 0;
 
 for (let i = n; i > 0; i = Math.floor(i / 10)) {
   r = r * 10 + Math.floor(i % 10);
@@ -9,3 +9,41 @@ for (let i = n; i > 0; i = Math.floor(i / 10)) {
 
 console.log(r);
 
+// Palindrme
+
+var n = 12321;
+var r = 0;
+
+for (let i = n; i > 0; i = Math.floor(i / 10)) {
+  r = r * 10 + Math.floor(i % 10);
+}
+
+console.log(r == n ? `yes` : `no`);
+
+// Strong Number
+
+function fact(i) {
+  if (i == 0 || i == 1) {
+    return 1;
+  }
+  return i * fact(i - 1);
+}
+
+var n = 145;
+var sum = 0;
+for (i = n; i > 0; i = parseInt(i / 10)) {
+  sum += fact(i % 10);
+}
+
+console.log(n == sum ? `yes` : "no");
+
+// AutoMorphic Number
+
+n = 625;
+var r = n * n;
+x = 1;
+for (i = n; i > 0; i = Math.floor(i / 10)) {
+  x *= 10;
+}
+
+console.log(r % x == n ? `yes` : `no`);
