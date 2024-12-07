b49e155c4189c803e65b6eb2a6f951fee65bdab5
aniket-dev-ai <2414501734@mujonline.edu.in>
2024-12-07 13:52:51 +0530

    initial

---
+++ Day_6/script.js
@@ -0,0 +1,9 @@
+let n = Number(prompt("Enter Right Angled Triangled Height"))
+
+for(let i =1;i<=n;i++){
+    let s = ""
+    for(let j = 1; j<=i;i++){
+        s += " *" 
+    }
+    console.log(s);
+}
