887cb4c72146d64f84979758bc784c8a94ba1bd0
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2024-12-30 13:55:41 +0530

    Time: 0 ms (100%), Space: 11 MB (45.32%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-bool zero(vector<int)& nums){
+bool zero(vector<int>& nums){
      int size = nums.size();
         for(int i = 0 ; i < size;i++){
             if(nums[i] != 0){
@@ -9,20 +9,33 @@ bool zero(vector<int)& nums){
         }
         return true;
 }
-void subs(vector<int) &nums , int min){
+void subs(vector<int> &nums , int min){
  int size = nums.size();
-        
+         for(int i = 0 ; i < size;i++){
+            if(nums[i]>=min){
+                nums[i] = nums[i]-min;
+            }
+         }
 }
-int min(vector<int)& a){
+int min(vector<int>& nums){
  int size = nums.size();
-        
+ int mini = nums[0];
+  for(int i = 0 ; i < size;i++){
+    if(nums[i]==0){continue;}
+    if(mini == 0 || mini>nums[i]){
+        mini = nums[i];
+    }
+  }
+        return mini;
 }
     int minimumOperations(vector<int>& nums) {
      int size = nums.size();
-        
+        int count =0;
         while(zero(nums) == 0){
             int minimum = min(nums);
             subs(nums , minimum);
+            count++;
         }
+        return count;
     }
 };
