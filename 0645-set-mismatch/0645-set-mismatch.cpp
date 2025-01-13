9a5d0e48287301211367c4bd8f86507f3f98e6fb
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-13 22:17:59 +0530

    Time: 0 ms (100%), Space: 26.4 MB (37.51%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,10 +1,23 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-     for(int i = 0 ; i < nums.size() ; i++){
-        if(nums.find(i) == string::npos){
-            
+        int n = nums.size();
+        int duplicate = -1, missing = -1;
+        vector<int> freq(n + 1, 0);
+ 
+        for (int num : nums) {
+            freq[num]++;
         }
-     }   
+ 
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) duplicate = i;
+            if (freq[i] == 0) missing = i;
+        }
+
+        return {duplicate, missing};
     }
 };
