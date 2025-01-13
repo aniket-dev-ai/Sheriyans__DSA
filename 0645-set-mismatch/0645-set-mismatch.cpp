eb9cd7fb17d23fc0d0723b23e027f090b5782c9d
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-13 22:05:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+     for(int i = 0 ; i < nums.size() ; i++){
+        if(nums.find(i) == string::npos){
+            
+        }
+     }   
+    }
+};
