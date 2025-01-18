f9c8c99d03248a96d24c79b17eab5e8c35a188c4
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-18 11:52:49 +0530

    LL day2

--- Day_14_linkedList/L1.cpp
+++ Day_14_linkedList/L1.cpp
@@ -1,18 +1,18 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 // Linked list ka basic structure define kar rahe hain
 class Node
 {
 public:
-    int data;      // Node ke andar data ko store karne ke liye variable
-    Node *next;     // Next pointer jo next node ka address store karega
+    int data;   // Node ke andar data ko store karne ke liye variable
+    Node *next; // Next pointer jo next node ka address store karega
 
     // Constructor: Jab bhi naye node ka object banate hain, yeh run hota hai
     Node(int value)
     {
-        this->data = value;  // Node ka data set karte hain
-        this->next = NULL;   // Next pointer ko initially NULL set karte hain
+        this->data = value; // Node ka data set karte hain
+        this->next = NULL;  // Next pointer ko initially NULL set karte hain
     }
 };
 
@@ -21,16 +21,16 @@ Node *insertAtHead(int value, Node *&head, Node *&tail)
 {
     // Agar linked list abhi empty hai (head aur tail dono NULL hain)
     if (head == NULL && tail == NULL)
-    { 
+    {
         Node *newNode = new Node(value); // Naya node banate hain aur uska data set karte hain
-        head = newNode; // Head ko naye node pr point karte hain
-        tail = newNode; // Tail bhi naye node pr point karega (kyunki sirf ek hi node hai)
+        head = newNode;                  // Head ko naye node pr point karte hain
+        tail = newNode;                  // Tail bhi naye node pr point karega (kyunki sirf ek hi node hai)
     }
     else
-    { 
+    {
         Node *newNode = new Node(value); // Ek naya node banate hain aur uska data set karte hain
-        newNode->next = head; // Naya node ab existing head node ke address pr point karega
-        head = newNode; // Head ko naye node par update karte hain
+        newNode->next = head;            // Naya node ab existing head node ke address pr point karega
+        head = newNode;                  // Head ko naye node par update karte hain
     }
 
     return head; // Updated head ko return karte hain
@@ -39,13 +39,79 @@ Node *insertAtHead(int value, Node *&head, Node *&tail)
 // Yeh function linked list ke sare nodes ka data print karega
 void print(Node *head)
 {
-    Node *temp = head; // Temporary pointer ko head pr set karte hain
+    Node *temp = head;   // Temporary pointer ko head pr set karte hain
     while (temp != NULL) // Jab tak pointer NULL na ho (matlab list ka end na aaye)
     {
         cout << temp->data << " "; // Current node ka data print karte hain
-        temp = temp->next; // Pointer ko next node pr move karte hain
+        temp = temp->next;         // Pointer ko next node pr move karte hain
     }
     cout << endl; // Line change karte hain for better output formatting
+}
+
+int getLength(Node *head)
+{
+
+    int len = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+Node *insertAtPosition(int position, int value, Node *head, Node *tail)
+{
+    Node *newNode = new Node(value);
+    
+    // Handle inserting at the head
+    if (position == 1)
+    {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+
+    Node *temp = head;
+    int count = 1;
+
+    // Traverse to find the position
+    while (temp != NULL && count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    // Check if position is valid
+    if (temp == NULL)
+    {
+        cout << "Invalid position" << endl;
+        return head;
+    }
+
+    // Insert the node at the position
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    // If inserting at the tail, update the tail pointer
+    if (newNode->next == NULL)
+    {
+        tail = newNode;
+    }
+
+    return head;
+}
+
+bool linearSearch(int value,Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        if(temp -> data == value){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false; 
 }
 
 // Program execution yahan se start hota hai
@@ -58,7 +124,15 @@ int main()
     head = insertAtHead(20, head, tail); // 20 ko head pr insert karte hain
     head = insertAtHead(30, head, tail); // 30 ko head pr insert karte hain
 
-    print(head); // Linked list ko print karte hain
+    // print(head); // Linked list ko print karte hain
+
+    cout << getLength(head)<<endl;
+    insertAtPosition(3,420,head,tail);
+    print(head);
+
+    cout<<linearSearch(20,head);
+    cout<<endl;
+    cout<<linearSearch(120,head);
 
     return 0; // Program successful run hone ke baad return 0 karte hain
 }
