5c423e4d2846c9e9278ee9c1d6372ef17327fcb6
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-21 11:28:19 +0530

    3

--- Day_13_Recusrion/Q2.cpp
+++ Day_13_Recusrion/Q2.cpp
@@ -10,8 +10,17 @@ int sumN(int n)
 
     return n + sumN(n - 1);
 }
+
+int SumOfDigit(int n)
+{
+    if (n <= 0 )
+        return 0;
+
+    return ((n % 10) + SumOfDigit(n / 10));
+}
 int main()
 {
     cout << sumN(5) << endl;
+    cout << SumOfDigit(7896) << endl;
     return 0;
 }
