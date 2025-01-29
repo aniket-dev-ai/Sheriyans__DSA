db233c5d34fa11a87c88ec89facc9d56ac606037
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-29 15:01:50 +0530

    Time: 0 ms (100%), Space: 11.6 MB (31.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> a, b, c;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == 0) a.push_back(nums[i]);
-            if(nums[i] == 1) b.push_back(nums[i]);
-            if(nums[i] == 2) c.push_back(nums[i]);
-        }
+        int low = 0, mid = 0, high = nums.size() - 1;
 
-        int i = 0;
-        for(; i < a.size(); i++) {
-            nums[i] = a[i];
-        }
-
-        for(int j = 0; j < b.size(); j++) {
-            nums[i++] = b[j];
-        }
-
-        for(int k = 0; k < c.size(); k++) {
-            nums[i++] = c[k];
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
     }
 };
