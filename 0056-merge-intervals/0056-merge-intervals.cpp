5cb0c92d39bbb64a7f5fb1778354aea71e8e54b7
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-02-01 13:11:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,17 +2,29 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
         vector<vector<int>> merged;
         
-        for (auto& interval : intervals) {
-            if (merged.empty() || merged.back()[1] < interval[0]) {
-                merged.push_back(interval);
-            } else {
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+        // Iterating over all intervals
+        for (int i = 0; i < intervals.size(); i++) {
+            bool mergedFlag = false;
+            // Compare with every other interval
+            for (int j = 0; j < merged.size(); j++) {
+                // If intervals overlap
+                if (intervals[i][0] <= merged[j][1] && intervals[i][1] >= merged[j][0]) {
+                    // Merge them by updating the end time
+                    merged[j][0] = min(merged[j][0], intervals[i][0]);
+                    merged[j][1] = max(merged[j][1], intervals[i][1]);
+                    mergedFlag = true;
+                    break;
+                }
+            }
+            // If no merge was possible, add the interval as a new one
+            if (!mergedFlag) {
+                merged.push_back(intervals[i]);
             }
         }
         
         return merged;
     }
 };
+
