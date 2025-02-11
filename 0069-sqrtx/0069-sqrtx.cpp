4dc29ef04dae8325a4c79bc5957f146afc4453ee
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-02-11 19:25:03 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,17 +3,18 @@ public:
     int mySqrt(int x) {
          if (x == 0 || x == 1) return x;
 
-    int start = 0, end = x, ans = 0;
+    int start = 0, end = x;
     while (start <= end) {
-        int mid = start + (end - start) / 2;
-
-        if (mid <= x / mid) {
-            ans = mid;
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+       long long  int mid = start + (end - start) / 2;
+        long long int m = mid * mid;
+        long long int m1 =( mid - 1) * ( mid - 1);
+        long long int m2 = ( mid + 1) * ( mid + 1);
+        if(m == x) return mid;
+        if(m1<x && m > x) return mid -1;
+        if(m2>x && m < x) return mid ;
+        if(m < x ) start = mid+1;
+        if(m > x ) end = mid -1 ;
     }
-    return ans;
+    return -1;
 }
 };
