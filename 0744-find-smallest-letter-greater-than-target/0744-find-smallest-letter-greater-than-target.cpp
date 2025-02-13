7948b5591ea9a053cc9fef09b4685f08babb6450
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-02-13 12:35:58 +0530

    Time: 135 ms (0.48%), Space: 20.8 MB (3.23%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<int> convert(vector<char>& l) {
-        vector<int> a;
-        for (int i = 0; i < l.size(); i++) {
-            int ia = (int)l[i];
-            a.push_back(ia);
-        }
-        return a;
+ vector<int> convert(vector<char>& l){
+    vector<int> a;
+    for(int i = 0 ; i<l.size();i++){
+        int ia = (int)l[i];
+        a.push_back(ia);
+        cout<<a[i]<<" ";
     }
+    return a;
+ }
 
     char nextGreatestLetter(vector<char>& li, char t) {
         vector<int> l = convert(li);
-        if ((int)t > l[l.size() - 1]) {
-            return li[0]; 
-        }
-
+     
         int s = 0, e = l.size() - 1;
+        int m = s + (e - s) / 2;
         while (s <= e) {
-            int m = s + (e - s) / 2;
             if (l[m] > (int)t) {
                 e = m - 1;
             } else {
                 s = m + 1;
             }
+            m = s + (e - s) / 2;
         }
-        return li[s % li.size()];
+        return li[s%li.size()];
     }
 };
