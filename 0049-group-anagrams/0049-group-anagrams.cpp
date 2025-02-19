f8ac3b63182d4d1b949641257c66a93e18066d9d
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-02-19 17:05:27 +0530

    Time: 26 ms (26.25%), Space: 26 MB (48.46%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> map;
+        vector<vector<string>> ans;
+        for(int i = 0 ; i<strs.size();i++){
+            string str = strs[i];
+            sort(str.begin(),str.end());
+             map[str].push_back(strs[i]);
+        }
+        for(auto i : map){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
