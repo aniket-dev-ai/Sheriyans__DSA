f4d7a8ac13fa1763ed701e422ca4d3f469d214d8
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-04-03 13:19:32 +0530

    mergetwosortedarray

---
+++ Day_46 Dnc and Backtracking/MergeTwoSortedArray.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Function to merge two sorted arrays
+
+void mergetwosortedarray(int arr1[], int arr2[], int n, int m, vector<int> &ans){
+    int i = 0, j = 0;
+    while(i<n&&j<m){
+        if(arr1[i]<arr2[j]){
+            ans.push_back(arr1[i]);
+            i++;
+        }
+        else{
+            ans.push_back(arr2[j]);
+            j++;
+        }
+
+        if(i==n){
+            while(j<m){
+                ans.push_back(arr2[j]);
+                j++;
+            }
+        }
+        else if(j==m){
+            while(i<n){
+                ans.push_back(arr1[i]);
+                i++;
+            }
+        }
+
+    }
+}
+ 
+
+int main()
+{
+
+    int arr1[] = {1, 3, 5, 7, 9};
+    int arr2[] = {2, 4, 6, 8, 10};
+    int n = sizeof(arr1) / sizeof(arr1[0]);
+    int m = sizeof(arr2) / sizeof(arr2[0]);
+    vector<int> ans;
+    mergetwosortedarray(arr1, arr2, n, m, ans);
+    cout << "Merged array: ";
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
